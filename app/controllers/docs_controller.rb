4d86110939c913f41975e4c67f2626b31586b660
salman <salmanqureshi3029@gmail.com>
2016-06-08 12:10:15 +0500

    Doc controller & routes

---
+++ app/controllers/docs_controller.rb
@@ -0,0 +1,40 @@
+class DocsController < ApplicationController
+	# index & show corresponds to Read part of the CRUD
+	def index
+		
+	end
+
+	def show
+		
+	end
+	#new & create responds to Create part
+	def new
+		
+	end
+	#doesnt have a view itself
+	def create
+		
+	end
+	# responds to Delete part
+	def destory
+		
+	end
+	# edit & update both respond to Update part
+	def edit
+		
+	end
+	#doesnt have view either
+	def update
+		
+	end
+
+	private
+		def find_docs
+			
+		end
+		def doc_params
+			
+		end
+
+
+end
