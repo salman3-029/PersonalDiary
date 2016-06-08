a84fcfef2730c6054cde8e7b7de10a3e9ad63a13
salman <salmanqureshi3029@gmail.com>
2016-06-08 12:45:00 +0500

    Add docs views & define actions

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -1,19 +1,26 @@
 class DocsController < ApplicationController
 	# index & show corresponds to Read part of the CRUD
+	before_action :find_doc, only: [:show,:edit,:update,:destroy]
+
 	def index
-		
+		@docs=Doc.all.order("created_at DESC")
 	end
 
 	def show
-		
+
 	end
 	#new & create responds to Create part
 	def new
-		
+		@doc= Doc.new
 	end
 	#doesnt have a view itself
 	def create
-		
+		@doc= Doc.new(doc_params)
+		if @doc.save
+			redirect_to @doc
+		else
+			rendder 'new'
+		end
 	end
 	# responds to Delete part
 	def destory
@@ -29,11 +36,11 @@ class DocsController < ApplicationController
 	end
 
 	private
-		def find_docs
-			
+		def find_doc
+			@doc=Doc.find(params[:id])
 		end
 		def doc_params
-			
+			params.require(:doc).permit(:title, :content)
 		end
 
 
