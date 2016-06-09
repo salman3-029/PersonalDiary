de36905688e54feffc9bb26e3411f6cab1b0a6cf
salman <salmanqureshi3029@gmail.com>
2016-06-09 11:28:47 +0500

    Association bw users & files

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -11,11 +11,11 @@ class DocsController < ApplicationController
 	end
 	#new & create responds to Create part
 	def new
-		@doc= Doc.new
+		@doc= current_user.docs.build
 	end
 	#doesnt have a view itself
 	def create
-		@doc= Doc.new(doc_params)
+		@doc= current_user.docs.build(doc_params)
 		if @doc.save
 			redirect_to @doc
 		else
